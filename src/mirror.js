6f23d496c88cc447523f9b04e9e481f4363686d4
plasma-node <19627847+plasma-node@users.noreply.github.com>
2023-06-16 19:31:33 -0700

    works, doesnt delete

--- src/mirror.js
+++ src/mirror.js
@@ -1,17 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-//const workingDir = process.cwd();
-/*
-const sourceDir = path.join(workingDir, 'Source'); // Replace with your source directory
-const destDir = path.join(workingDir, 'Destination'); // Replace with your destination directory
-const interval = 2000; // Interval in milliseconds (e.g., 2000ms = 2 seconds)
-*/
-
 var mirroredPairs = [];
-function mirrorFiles(sourceDir, destDir) {
-  //console.log('Current directory:', process.cwd());
 
+function mirrorFiles(sourceDir, destDir) {
   fs.readdir(sourceDir, (err, files) => {
     if (err) {
       console.error(`Error reading directory ${sourceDir}:`, err);
@@ -19,9 +11,8 @@ function mirrorFiles(sourceDir, destDir) {
     }
 
     files.forEach(file => {
-        console.log("for file: " + file)
       const sourceFile = path.join(sourceDir, file);
-      const destFile = path.join(destDir, path.relative(sourceDir, sourceFile));
+      const destFile = path.join(destDir, file);
 
       fs.stat(sourceFile, (err, sourceStats) => {
         if (err) {
@@ -29,10 +20,8 @@ function mirrorFiles(sourceDir, destDir) {
           return;
         }
 
-        console.log("stat");
-
         if (sourceStats.isDirectory()) {
-          const nestedDestDir = path.join(destDir, path.relative(sourceDir, sourceFile));
+          const nestedDestDir = path.join(destDir, file);
 
           fs.stat(nestedDestDir, (err, destStats) => {
             if (err && err.code === 'ENOENT') {
@@ -77,79 +66,69 @@ function mirrorFiles(sourceDir, destDir) {
 }
 
 function checkDirectoriesExist(source, destination) {
-    const sourceExists = fs.existsSync(source);
-    const destinationExists = fs.existsSync(destination);
-  
-    if (sourceExists && destinationExists) {
-        return true;
-      //console.log(`Both source directory '${source}' and destination directory '${destination}' exist.`);
-    } else {
-      console.log(`One or both of the directories do not exist.`);
-      if (!sourceExists) {
-        console.log(`Source directory '${source}' does not exist.`);
-      }
-      if (!destinationExists) {
-        console.log(`Destination directory '${destination}' does not exist.`);
-      }
-      return false;
+  const sourceExists = fs.existsSync(source);
+  const destinationExists = fs.existsSync(destination);
+
+  if (sourceExists && destinationExists) {
+    return true;
+  } else {
+    console.log(`One or both of the directories do not exist.`);
+    if (!sourceExists) {
+      console.log(`Source directory '${source}' does not exist.`);
     }
+    if (!destinationExists) {
+      console.log(`Destination directory '${destination}' does not exist.`);
+    }
+    return false;
   }
-  
-  
+}
+
 function readFileAndProcess(file, root, callback) {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file ${file}:`, err);
+      return;
+    }
 
-    fs.readFile(file, 'utf8', (err, data) => {
-            if (err) {
-                console.error(`Error reading file ${file}:`, err);
-                return;
-            }
-
-        const pairs = data.split('\n');
-        pairs.forEach(pair => {
-            let [source, destination] = pair.split('->').map(str => str.trim());
-            source = root + "/" + source;
-            destination = root + "/" + destination;
-            console.log("source: " + source + " dest: " + destination);
-            callback(source, destination);
-        });
+    const pairs = data.split('\n');
+    pairs.forEach(pair => {
+      let [source, destination] = pair.split('->').map(str => str.trim());
+      source = path.join(root, source);
+      destination = path.join(root, destination);
+      callback(source, destination);
     });
+  });
 }
-  
-  // Example callback function
-function processDirectories(source, destination) {
-      if (!checkDirectoriesExist(source, destination)) { 
-          console.log("failed"); return; 
-      };
-    console.log(`Source: ${source}, Destination: ${destination}`);
-    // Call your function or logic here for each source and destination pair
-    mirroredPairs.push([source, destination]);
-}
-  
-  // Usage
 
-var intervalId;
+function processDirectories(source, destination) {
+  if (!checkDirectoriesExist(source, destination)) {
+    return;
+  }
+
+  console.log(`Source: ${source}, Destination: ${destination}`);
+  mirroredPairs.push([source, destination]);
+}
+
 function startMirror(config, root, interval = 1000) {
-    readFileAndProcess(config, root, processDirectories);
-    //intervalId = setInterval(mirrorFiles, interval);
-    intervalId = setInterval(() => {
-        console.log("interval")
-        mirroredPairs.forEach((paired) => {
-            console.log("mirrored: ", paired);
-            mirrorFiles(paired[1], paired[2]);
-        });
-    }, interval);
-    
+  readFileAndProcess(config, root, processDirectories);
+
+  setInterval(() => {
+    mirroredPairs.forEach((paired) => {
+      mirrorFiles(paired[0], paired[1]);
+    });
+  }, interval);
 }
 
 function stopMirror() {
-    clearInterval(intervalId);
+  mirroredPairs = [];
 }
 
-function testMessage () {
-    return " mirror test message"
+function testMessage() {
+  return "Mirror test message";
 }
-//stopMirror();
 
 module.exports = {
-    startMirror, stopMirror, testMessage
+  startMirror,
+  stopMirror,
+  testMessage
 };
