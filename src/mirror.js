9e28d6a172e7ecc532a4426cf666923e12284ee1
plasma-node <19627847+plasma-node@users.noreply.github.com>
2023-06-16 19:42:15 -0700

    delete sorta work

--- src/mirror.js
+++ src/mirror.js
@@ -4,66 +4,102 @@ const path = require('path');
 var mirroredPairs = [];
 
 function mirrorFiles(sourceDir, destDir) {
-  fs.readdir(sourceDir, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory ${sourceDir}:`, err);
-      return;
-    }
-
-    files.forEach(file => {
-      const sourceFile = path.join(sourceDir, file);
-      const destFile = path.join(destDir, file);
-
-      fs.stat(sourceFile, (err, sourceStats) => {
+    fs.readdir(sourceDir, (err, files) => {
+      if (err) {
+        console.error(`Error reading directory ${sourceDir}:`, err);
+        return;
+      }
+  
+      files.forEach(file => {
+        const sourceFile = path.join(sourceDir, file);
+        const destFile = path.join(destDir, file);
+  
+        fs.stat(sourceFile, (err, sourceStats) => {
+          if (err) {
+            console.error(`Error getting stats for ${sourceFile}:`, err);
+            return;
+          }
+  
+          if (sourceStats.isDirectory()) {
+            const nestedDestDir = path.join(destDir, file);
+  
+            fs.stat(nestedDestDir, (err, destStats) => {
+              if (err && err.code === 'ENOENT') {
+                fs.mkdir(nestedDestDir, { recursive: true }, err => {
+                  if (err) {
+                    console.error(`Error creating directory ${nestedDestDir}:`, err);
+                  } else {
+                    console.log(`Created directory: ${nestedDestDir}`);
+                    mirrorFiles(sourceFile, nestedDestDir); // Recursively mirror files in nested directory
+                  }
+                });
+              } else if (!err && destStats.isDirectory()) {
+                mirrorFiles(sourceFile, nestedDestDir); // Directory already exists, recursively mirror files in nested directory
+              }
+            });
+          } else {
+            fs.stat(destFile, (err, destStats) => {
+              if (err && err.code === 'ENOENT') {
+                // File doesn't exist in destination, so copy it
+                fs.copyFile(sourceFile, destFile, err => {
+                  if (err) {
+                    console.error(`Error copying ${sourceFile} to ${destFile}:`, err);
+                  } else {
+                    console.log(`Copied ${sourceFile} to ${destFile}`);
+                  }
+                });
+              } else if (sourceStats.mtimeMs > destStats.mtimeMs) {
+                // Source file is newer than destination file, so update it
+                fs.copyFile(sourceFile, destFile, err => {
+                  if (err) {
+                    console.error(`Error updating ${destFile} with ${sourceFile}:`, err);
+                  } else {
+                    console.log(`Updated ${destFile} with ${sourceFile}`);
+                  }
+                });
+              }
+            });
+          }
+        });
+      });
+  
+      // Check for files in the destination directory that don't exist in the source directory
+      fs.readdir(destDir, (err, destFiles) => {
         if (err) {
-          console.error(`Error getting stats for ${sourceFile}:`, err);
+          console.error(`Error reading directory ${destDir}:`, err);
           return;
         }
-
-        if (sourceStats.isDirectory()) {
-          const nestedDestDir = path.join(destDir, file);
-
-          fs.stat(nestedDestDir, (err, destStats) => {
+  
+        destFiles.forEach(destFile => {
+          const sourceFile = path.join(sourceDir, destFile);
+          const destFilePath = path.join(destDir, destFile);
+  
+          fs.stat(sourceFile, (err) => {
             if (err && err.code === 'ENOENT') {
-              fs.mkdir(nestedDestDir, { recursive: true }, err => {
-                if (err) {
-                  console.error(`Error creating directory ${nestedDestDir}:`, err);
-                } else {
-                  console.log(`Created directory: ${nestedDestDir}`);
-                  mirrorFiles(sourceFile, nestedDestDir); // Recursively mirror files in nested directory
-                }
-              });
-            } else if (!err && destStats.isDirectory()) {
-              mirrorFiles(sourceFile, nestedDestDir); // Directory already exists, recursively mirror files in nested directory
+              if (fs.statSync(destFilePath).isDirectory()) {
+                fs.rmdir(destFilePath, { recursive: true }, err => {
+                  if (err) {
+                    console.error(`Error deleting directory ${destFilePath}:`, err);
+                  } else {
+                    console.log(`Deleted directory ${destFilePath}`);
+                  }
+                });
+              } else {
+                fs.unlink(destFilePath, err => {
+                  if (err) {
+                    console.error(`Error deleting file ${destFilePath}:`, err);
+                  } else {
+                    console.log(`Deleted file ${destFilePath}`);
+                  }
+                });
+              }
             }
           });
-        } else {
-          fs.stat(destFile, (err, destStats) => {
-            if (err && err.code === 'ENOENT') {
-              // File doesn't exist in destination, so copy it
-              fs.copyFile(sourceFile, destFile, err => {
-                if (err) {
-                  console.error(`Error copying ${sourceFile} to ${destFile}:`, err);
-                } else {
-                  console.log(`Copied ${sourceFile} to ${destFile}`);
-                }
-              });
-            } else if (sourceStats.mtimeMs > destStats.mtimeMs) {
-              // Source file is newer than destination file, so update it
-              fs.copyFile(sourceFile, destFile, err => {
-                if (err) {
-                  console.error(`Error updating ${destFile} with ${sourceFile}:`, err);
-                } else {
-                  console.log(`Updated ${destFile} with ${sourceFile}`);
-                }
-              });
-            }
-          });
-        }
+        });
       });
     });
-  });
-}
+  }
+  
 
 function checkDirectoriesExist(source, destination) {
   const sourceExists = fs.existsSync(source);
